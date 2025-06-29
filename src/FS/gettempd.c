14d0d83dceda0f3af1a517d87ded304ab74bf9e2
Ika <ikaaseek@gmail.com>
2025-06-29 04:36:20 +0200

    update filesystem

--- src/FS/gettempd.c
+++ src/FS/gettempd.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-FPKG_API_PUBLIC void FS_mktempd(UCHAR *buf, size_t buf_max, size_t n) {
+FPKG_API_PUBLIC void FS_gettempd(char *buf, size_t buf_max, size_t n) {
   char chars[] = "abcdefghijklmnoprstquwxyz"
                  "ABCDEFGHIJKLMNOPRSTQUWXYZ"
                  "0123456789";
@@ -14,14 +14,16 @@ FPKG_API_PUBLIC void FS_mktempd(UCHAR *buf, size_t buf_max, size_t n) {
   srand(tm);
 
   snprintf(buf, buf_max -= sizeof("/tmp/"), "/tmp/");
-  UCHAR *buf_next = buf + sizeof("/tmp/");
+  char *buf_next = buf + sizeof("/tmp/");
 
   for (size_t i = 0; i < n; ++i) {
-    if (i == buf_max) {
+    if (buf_max == 0) {
       buf_next[i] = '\0';
       break;
     }
     buf_next[i] = chars[rand() % sizeof(chars)];
     buf_max--;
   }
+
+  *(char *)((uintptr_t)buf_max + n) = '\0';
 }
